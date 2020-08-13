992f83ba310bec4d551bbec5388f5b212a63919e
Alisa Poon <alisa.poon97@gmail.com>
2020-08-13 07:40:23 -0700

    add sunrise, sunset, feels like

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,44 +1,46 @@
 $(document).ready(function() {
 
-  $('#today-date').text(`${moment().format('dddd, LL')}`);
+  $('#today-date').text(`${moment().format('dddd, l')}`);
   for (let i = 1; i < 6; i++) {
     let $div = $('<div>');
     let $innerDiv = $('<div>').addClass('uk-card uk-card-default uk-card-body');
-    let $h4 = $('<h4>').addClass("uk-card-title uk-text-center").text(moment().add(i, 'day').format('dddd, l'));
-    let $img = $('<img>').attr("alt", "weather icon").attr("id", "icon-" + i);
-    let $pTemp = $('<p>').text('High of the Day: ').append($('<span>').attr('id', 'temp-' + i));
+    let $h4 = $('<h4>').addClass('uk-card-title uk-text-center uk-text-light').text(moment().add(i, 'day').format('dddd, l'));
+    let $img = $('<img>').attr('alt', 'weather icon').attr('id', 'icon-' + i);
+    let $pDescr = $('<p>').addClass('uk-text-uppercase uk-text-small').attr('id', 'descr-' + i);
+    let $pTemp = $('<p>').text('Temperature: ').append($('<span>').attr('id', 'temp-' + i));
     let $pHumidity = $('<p>').text('Humidity: ').append($('<span>').attr('id', 'humidity-' + i));
-    $div.append($innerDiv.append($h4.append('<br>', $img), $pTemp, $pHumidity));
+    $div.append($innerDiv.append($h4.append('<br>', $pDescr, $img), $pTemp, $pHumidity));
     $('.uk-grid-match').append($div);
   }
 
   let cities = ["Austin", "Chicago", "New York", "Orlando", "San Francisco", "Seattle", "Denver", "Atlanta"];
   
-  function renderCities() {
+  function renderCityList() {
     $("#view-city-cards").empty();
     for (let i = 0; i < cities.length; i++) {
       let $d = $("<div>").addClass("uk-card-header").attr("data-index", i).attr("id", "city-" + i);
       let $a = $("<a>").attr("href", "#").addClass("uk-button uk-button-text city-link").text(cities[i]);
-      let $b = $("<button>").addClass("uk-button uk-button-text uk-float-right trash").attr("uk-icon", "trash");
+      let $b = $("<button>").addClass("uk-button uk-button-text uk-float-right trash").attr("uk-icon", "close");
       $d.append($a, $b);
       $("#view-city-cards").append($d);
     }
   }
 
-  renderCities();
+  renderCityList();
 
   $("#add-city").on("click", function(event) {
     event.preventDefault();
     let city = $("#city-input").val().trim();
     cities.unshift(city);
     $("#city-input").val("");
-    renderCities();
+    renderCityList();
+    $('#city-0').children().first().click();
   });
 
   $(document).on("click", ".trash", function(event) {
     const index = event.target.parentElement.parentElement.getAttribute("data-index");
     cities.splice(index, 1);
-    renderCities();
+    renderCityList();
   });
 
   $(document).on("click", ".city-link", displayInfo);
@@ -49,15 +51,20 @@ $(document).ready(function() {
     let tempUnit = "imperial";
     let APIKey = "166a433c57516f51dfab1f7edaed8413";
     $.ajax({
-        url: "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=" + tempUnit + "&appid=" + APIKey,
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=" + tempUnit + "&set=unix&appid=" + APIKey,
         method: "GET"
     }).then(function(response) {
+      console.log(response);
       $('#current-city').text(`${response.name}, ${response.sys.country}`)
-      $('#current-city').append($('<span>').addClass("uk-float-right").text(`${moment().format('dddd, LL')}`));
+      $('#current-city').append($('<span>').addClass("uk-float-right uk-text-light").text(`${moment().format('dddd, l')}`));
+      $('.current-description').html(response.weather[0].description);
       $('#today-icon').attr('src', `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`)
-      $('#today-temp').text(`${response.main.feels_like}°F`);
+      $('#today-temp').text(`${response.main.temp}°F`);
+      $('#feels-like').text(`${response.main.feels_like}°F`);
       $('#today-humidity').text(`${response.main.humidity}%`);
       $('#today-wind').text(`${response.wind.speed} MPH`);
+      $('#sunrise').text(moment(response.sys.sunrise * 1000).format('LT'));
+      $('#sunset').text(moment(response.sys.sunset * 1000).format('LT'));
       let lat = response.coord.lat;
       let lon = response.coord.lon;
       $.ajax({
@@ -78,21 +85,13 @@ $(document).ready(function() {
       url: "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + cityName + "&units=" + tempUnit + "&appid=" + APIKey,
       method: "GET"
     }).then(function(response) {
-      $('#icon-1').attr("src", `http://openweathermap.org/img/wn/${response.list[0].weather[0].icon}@2x.png`);
-      $('#temp-1').text(`${response.list[0].temp.max}°F`);
-      $('#humidity-1').text(`${response.list[0].humidity}%`);
-      $('#icon-2').attr("src", `http://openweathermap.org/img/wn/${response.list[1].weather[0].icon}@2x.png`);
-      $('#temp-2').text(`${response.list[1].temp.max}°F`);
-      $('#humidity-2').text(`${response.list[1].humidity}%`);
-      $('#icon-3').attr("src", `http://openweathermap.org/img/wn/${response.list[2].weather[0].icon}@2x.png`);
-      $('#temp-3').text(`${response.list[2].temp.max}°F`);
-      $('#humidity-3').text(`${response.list[2].humidity}%`);
-      $('#icon-4').attr("src", `http://openweathermap.org/img/wn/${response.list[3].weather[0].icon}@2x.png`);
-      $('#temp-4').text(`${response.list[3].temp.max}°F`);
-      $('#humidity-4').text(`${response.list[3].humidity}%`);
-      $('#icon-5').attr("src", `http://openweathermap.org/img/wn/${response.list[4].weather[0].icon}@2x.png`);
-      $('#temp-5').text(`${response.list[4].temp.max}°F`);
-      $('#humidity-5').text(`${response.list[4].humidity}%`);
+      console.log(response);
+      for (let i = 0; i < 5; i++) {
+        $(`#descr-${i+1}`).text(response.list[i].weather[0].description)
+        $(`#icon-${i+1}`).attr("src", `http://openweathermap.org/img/wn/${response.list[i].weather[0].icon}@2x.png`);
+        $(`#temp-${i+1}`).text(`${Math.floor(response.list[i].temp.max)}°F / ${Math.floor(response.list[i].temp.min)}°F`);
+        $(`#humidity-${i+1}`).text(`${response.list[i].humidity}%`);
+      }
     });
   };
   
