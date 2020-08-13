28e08fcd01ff4dfa6c84bc09bee1761f97b904f2
Alisa Poon <alisa.poon97@gmail.com>
2020-08-13 00:21:04 -0700

    forecast working

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,69 +1,95 @@
-// let APIKey = "166a433c57516f51dfab1f7edaed8413";
-// let place = {city: "London"}
-// let tempUnit = "imperial"
-// var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + place.city + "&units=" + tempUnit + "&appid=" + APIKey;
-// $.ajax({
-//   url: queryURL,
-//   method: "GET"
-// }).then(function(response) {
-//   console.log(response);
-// });
-let cities = ["Austin", "Chicago", "New York", "Orlando", "San Francisco", "Seattle", "Denver", "Atlanta"];
-function renderCities() {
-  $("#view-city-cards").empty();
-  for (let i = 0; i < cities.length; i++) {
-    let $d = $("<div>").addClass("uk-card-header").attr("data-index", i);
-    let $a = $("<a>").attr("href", "#").addClass("uk-button uk-button-text city-link").text(cities[i]);
-    let $b = $("<button>").addClass("uk-button uk-button-text uk-float-right trash").attr("uk-icon", "trash");
-    $d.append($a, $b);
-    $("#view-city-cards").append($d);
+$(document).ready(function() {
+    {/* <div>
+          <div class="uk-card uk-card-default uk-card-body">
+            <h4 class="uk-card-title">Next Date</h4>
+            <p>Temperature: <span id="temp-1"></span></p>
+            <p>Humidity: <span id="humidity-1"></span></p>
+          </div>
+        </div>
+        <div>
+          <div class="uk-card uk-card-default uk-card-body">
+            <h4 class="uk-card-title">Next Date</h4>
+            <p>Temperature: <span id="temp-2"></span></p>
+            <p>Humidity: <span id="humidity-2"></span></p>
+          </div>
+        </div> */}
+  $('#today-date').text(`(${moment().format('L')}`);
+  for (let i = 1; i < 6; i++) {
+    let $div = $('<div>');
+    let $innerDiv = $('<div>').addClass('uk-card uk-card-default uk-card-body');
+    let $h4 = $('<h4>').addClass("uk-card-title").text(moment().add(i, 'day').format('L'));
+    let $pTemp = $('<p>').text('Temperature: ').append($('<span>').attr('id', 'temp-' + i));
+    let $pHumidity = $('<p>').text('Humidity: ').append($('<span>').attr('id', 'humidity-' + i));
+    $div.append($innerDiv.append($h4, $pTemp, $pHumidity));
+    $('.uk-grid-match').append($div);
   }
-}
-renderCities();
-$("#add-city").on("click", function(event) {
-  event.preventDefault();
-  let city = $("#city-input").val().trim();
-  cities.unshift(city);
+  let cities = ["Austin", "Chicago", "New York", "Orlando", "San Francisco", "Seattle", "Denver", "Atlanta"];
+  function renderCities() {
+    $("#view-city-cards").empty();
+    for (let i = 0; i < cities.length; i++) {
+      let $d = $("<div>").addClass("uk-card-header").attr("data-index", i);
+      let $a = $("<a>").attr("href", "#").addClass("uk-button uk-button-text city-link").text(cities[i]);
+      let $b = $("<button>").addClass("uk-button uk-button-text uk-float-right trash").attr("uk-icon", "trash");
+      $d.append($a, $b);
+      $("#view-city-cards").append($d);
+    }
+  }
   renderCities();
-});
+  $("#add-city").on("click", function(event) {
+    event.preventDefault();
+    let city = $("#city-input").val().trim();
+    cities.unshift(city);
+    renderCities();
+  });
 
-$(document).on("click", ".city-link", function(event) {
-  event.preventDefault();
-  const cityName = event.target.parentElement.textContent;
-  let tempUnit = "imperial";
-  let APIKey = "166a433c57516f51dfab1f7edaed8413";
-  $.ajax({
-      url: "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=" + tempUnit + "&appid=" + APIKey,
-      method: "GET"
-  }).then(function(response) {
-    $('#current-city').text(response.name);
-    $('#today-temp').text(response.main.feels_like);
-    $('#today-humidity').text(response.main.humidity);
-    $('#today-wind').text(response.wind.speed);
-    let lat = response.coord.lat;
-    let lon = response.coord.lon;
+  $(document).on("click", ".city-link", function(event) {
+    event.preventDefault();
+    const cityName = event.target.parentElement.textContent;
+    let tempUnit = "imperial";
+    let APIKey = "166a433c57516f51dfab1f7edaed8413";
     $.ajax({
-      url: "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey,
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=" + tempUnit + "&appid=" + APIKey,
+        method: "GET"
+    }).then(function(response) {
+      $('#current-city').text(`${response.name} (${moment().format('L')})`);
+      $('#today-temp').text(`${response.main.feels_like}°F`);
+      $('#today-humidity').text(`${response.main.humidity}%`);
+      $('#today-wind').text(response.wind.speed);
+      let lat = response.coord.lat;
+      let lon = response.coord.lon;
+      $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey,
+        method: "GET"
+      }).then(function(response) {
+        $('#today-uv').text(response.value);
+        if (response.value < 3) {
+          $('#today-uv').attr('class', 'uk-alert-success');
+        } else if (response.value >= 3 && response.value < 8) {
+          $('#today-uv').attr('class', 'uk-alert-warning');
+        } else {
+          $('#today-uv').attr('class', 'uk-alert-danger');
+        }
+      });
+    });
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=" + tempUnit + "&appid=" + APIKey,
       method: "GET"
     }).then(function(response) {
-      $('#today-uv').text(response.value);
+      $('#temp-1').text(`${response.list[4].main.temp_max}°F`);
+      $('#humidity-1').text(`${response.list[4].main.humidity}%`);
+      $('#temp-2').text(`${response.list[12].main.temp_max}°F`);
+      $('#humidity-2').text(`${response.list[12].main.humidity}%`);
+      $('#temp-3').text(`${response.list[20].main.temp_max}°F`);
+      $('#humidity-3').text(`${response.list[20].main.humidity}%`);
+      $('#temp-4').text(`${response.list[28].main.temp_max}°F`);
+      $('#humidity-4').text(`${response.list[28].main.humidity}%`);
+      $('#temp-5').text(`${response.list[36].main.temp_max}°F`);
+      $('#humidity-5').text(`${response.list[36].main.humidity}%`);
     });
   });
-  $.ajax({
-    url: "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=" + tempUnit + "&appid=" + APIKey,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
+  $(document).on("click", ".trash", function(event) {
+    const index = event.target.parentElement.getAttribute("data-index");
+    cities.splice(index, 1);
+    renderCities();
   });
 });
-
-$(document).on("click", ".trash", function(event) {
-  const index = event.target.parentElement.getAttribute("data-index");
-  cities.splice(index, 1);
-  renderCities();
-});
-{/* <div class="uk-card-default" id="view-city-cards">
-<div class="uk-card-header">
-  <a href="#" class="uk-button uk-button-text">Austin</a> 
-  <button class="uk-button uk-button-text uk-float-right" uk-icon="icon: trash"></button>
-</div> */}
