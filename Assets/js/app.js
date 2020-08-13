92a9fc120cbdc4013600edfcd47bed98bdecccd2
Alisa Poon <alisa.poon97@gmail.com>
2020-08-13 03:09:57 -0700

    trigger first button when the browser loads

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,27 +1,32 @@
 $(document).ready(function() {
-  $('#today-date').text(`(${moment().format('dddd, M/D/YYYY')})`);
+
+  $('#today-date').text(`${moment().format('dddd, LL')}`);
   for (let i = 1; i < 6; i++) {
     let $div = $('<div>');
     let $innerDiv = $('<div>').addClass('uk-card uk-card-default uk-card-body');
-    let $h4 = $('<h4>').addClass("uk-card-title").text(moment().add(i, 'day').format('dddd, M/D/YYYY'));
+    let $h4 = $('<h4>').addClass("uk-card-title uk-text-center").text(moment().add(i, 'day').format('dddd, l'));
     let $img = $('<img>').attr("alt", "weather icon").attr("id", "icon-" + i);
     let $pTemp = $('<p>').text('High of the Day: ').append($('<span>').attr('id', 'temp-' + i));
     let $pHumidity = $('<p>').text('Humidity: ').append($('<span>').attr('id', 'humidity-' + i));
-    $div.append($innerDiv.append($h4, $img, $pTemp, $pHumidity));
+    $div.append($innerDiv.append($h4.append('<br>', $img), $pTemp, $pHumidity));
     $('.uk-grid-match').append($div);
   }
+
   let cities = ["Austin", "Chicago", "New York", "Orlando", "San Francisco", "Seattle", "Denver", "Atlanta"];
+  
   function renderCities() {
     $("#view-city-cards").empty();
     for (let i = 0; i < cities.length; i++) {
-      let $d = $("<div>").addClass("uk-card-header").attr("data-index", i);
+      let $d = $("<div>").addClass("uk-card-header").attr("data-index", i).attr("id", "city-" + i);
       let $a = $("<a>").attr("href", "#").addClass("uk-button uk-button-text city-link").text(cities[i]);
       let $b = $("<button>").addClass("uk-button uk-button-text uk-float-right trash").attr("uk-icon", "trash");
       $d.append($a, $b);
       $("#view-city-cards").append($d);
     }
   }
+
   renderCities();
+
   $("#add-city").on("click", function(event) {
     event.preventDefault();
     let city = $("#city-input").val().trim();
@@ -29,12 +34,16 @@ $(document).ready(function() {
     $("#city-input").val("");
     renderCities();
   });
+
   $(document).on("click", ".trash", function(event) {
     const index = event.target.parentElement.parentElement.getAttribute("data-index");
     cities.splice(index, 1);
     renderCities();
   });
-  $(document).on("click", ".city-link", function(event) {
+
+  $(document).on("click", ".city-link", displayInfo);
+  
+  function displayInfo(event) {
     event.preventDefault();
     const cityName = event.target.parentElement.textContent;
     let tempUnit = "imperial";
@@ -43,7 +52,8 @@ $(document).ready(function() {
         url: "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=" + tempUnit + "&appid=" + APIKey,
         method: "GET"
     }).then(function(response) {
-      $('#current-city').text(`${response.name} (${moment().format('dddd, M/D/YYYY')})`);
+      $('#current-city').text(`${response.name}, ${response.sys.country}`)
+      $('#current-city').append($('<span>').addClass("uk-float-right").text(`${moment().format('dddd, LL')}`));
       $('#today-icon').attr('src', `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`)
       $('#today-temp').text(`${response.main.feels_like}°F`);
       $('#today-humidity').text(`${response.main.humidity}%`);
@@ -84,5 +94,8 @@ $(document).ready(function() {
       $('#temp-5').text(`${response.list[36].main.temp_max}°F`);
       $('#humidity-5').text(`${response.list[36].main.humidity}%`);
     });
-  });
+  };
+  
+  $('#city-0').children().first().click();
+
 });
